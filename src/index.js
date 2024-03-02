ea4595f293a00cdc185a3df22bc92cc8800daff8
kc <krishnamalireddy7@gmail.com>
2024-03-02 19:03:03 +0530

    updated routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App'; // Replace with the path to your main app component
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
