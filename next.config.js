1db634d9aa779af1e6e7d8d0e6cbd26c3f342df7
kc <krishnamalireddy7@gmail.com>
2024-03-04 03:55:46 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export',
- 
+module.exports = {
   reactStrictMode: true,
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  output: 'export',
+
 }
- 
-export default nextConfig
